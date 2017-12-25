19d1967451878b22cd3af7dd3771cafd3a331295
aahladmadireddy <aahladmn99@gmail.com>
2017-12-25 00:23:52 -0600

    News page is mostly completed, some minor css changes to go. Added support for listing by author, date, category, or anything else.

--- build/wp-content/themes/nsr/home.php
+++ build/wp-content/themes/nsr/home.php
@@ -1,38 +1,38 @@
 <?php // News Page
-get_header();
+get_header(); ?>
 
-if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  <div class="container">
-    <div class="row">
+<div class="container news-listing">
+  <?php
+  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="row news-listing-item">
       <div class="col-sm-8 news-post">
-        <?php get_template_part('template-parts/content/content', get_post_format()); ?>
+        <?php get_template_part('template-parts/content/news-listing', get_post_format()); ?>
       </div>
 
       <?php if (is_latest()): ?>
-      <div class="col-sm-4 categories-panel">
-        <h4>Categories</h4>
-        <?php
-        $categories = get_categories(array(
-          'orderby' => 'name',
-          'order'   => 'ASC'
-        ));
+        <div class="col-sm-4 categories-panel">
+          <h4>Categories</h4>
+          <?php $categories = get_categories(array('orderby' => 'name', 'order'   => 'ASC')); ?>
 
-        foreach ($categories as $category): ?>
+          <ul class="categories-list">
+            <?php foreach ($categories as $category): ?>
+              <li><a href="<?php echo get_category_link($category) ?>"><?php  echo $category->name ?></a></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      <?php endif;?>
+    </div>
+  <?php endwhile; ?>
 
-          <a href="<?php echo get_category_link($category) ?>"><?php  echo $category->name ?></a>
-
-        <?php endforeach; endif;?>
-
-
-  </div>
+  <div class="row pagination-container">
+    <!--<div class="nav-previous alignleft"><?php /*next_posts_link( 'Older posts' ); */?></div>
+    <div class="nav-next alignright"><?php /*previous_posts_link( 'Newer posts' ); */?></div>-->
+    <div class="col-sm-8 text-center">
+      <?php wpbeginner_numeric_posts_nav() ?>
     </div>
   </div>
-
-<?php endwhile; ?>
-
-  <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
-  <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+</div>
 
 <?php endif;
 
---
+++ build/wp-content/themes/nsr/template-parts/content/news-listing.php
@@ -0,0 +1,12 @@
+<div>
+  <?php if (catch_that_image()) { ?>
+    <img src="<?php echo catch_that_image() ?>" alt="" class="img-responsive">
+  <?php } ?>
+  <h3 class="news-listing-title"><a href="<?php echo get_post_permalink(get_the_ID()) ?>"><?php the_title() ?></a></h3>
+  <p class="news-listing-meta"><?php the_date(); ?> by <?php the_author() ?></p>
+
+  <p><?php echo get_the_excerpt() ?></p>
+  <p><a href="<?php echo get_post_permalink(get_the_ID()) ?>">Read more</a></p>
+
+  <hr>
+</div>
